a4bda2831444ed69baa843403336ab38ea614691
Aiden <marcin.madry@interia.com>
2023-11-19 12:44:34 +0100

    simple and crude Anti-Aliasing

--- code/render.cpp
+++ code/render.cpp
@@ -62,10 +62,10 @@ internal SDL_Window *render_create_window(const char *window_name, unsigned int 
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
-
+    
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS, 1);
+    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 2);
+    
     SDL_Window *window = SDL_CreateWindow(window_name,
                                           SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                                           w, h,
